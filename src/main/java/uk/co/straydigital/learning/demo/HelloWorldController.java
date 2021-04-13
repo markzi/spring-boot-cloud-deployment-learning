9ef834b21d747b662ba374e733bcd3425abfbb4f
markzi <markzi@gmail.com>
2021-04-13 21:52:28 +0100

    testing jenkins

--- src/main/java/uk/co/straydigital/learning/demo/HelloWorldController.java
+++ src/main/java/uk/co/straydigital/learning/demo/HelloWorldController.java
@@ -1,6 +1,7 @@
 package uk.co.straydigital.learning.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class HelloWorldController {
     @GetMapping
     public String get() {
         return "Hello World!";
+    }
+
+    @GetMapping(value = "/name/{name}")
+    public String getWithName(@PathVariable("name") String name) {
+        return String.format("Hello %s", name);
     }
 }
